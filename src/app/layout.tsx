3f5c8f641ea96f9e7c9af22ea99057ec176495c0
bharatlal <bharatlalsohna@gmail.com>
2026-07-27 15:44:27 +0530

    add and change some in design and in amount for indian rupees and add image with cloudnary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import { AuthProvider } from '@/components/providers/auth-provider';
 export const metadata: Metadata = {
   title: 'NovaCart',
   description: 'Modern premium eCommerce experience built with Next.js',
+   icons: {
+    icon: "/icon2.png",
+    shortcut: "/icon2.png", 
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
